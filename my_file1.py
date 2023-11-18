8105c8cc010250e46959650a7e657453d4a1be03
JesusPaucarE <151387205+JesusPaucarE@users.noreply.github.com>
2023-11-18 18:29:45 -0300

    Update my_file1.py

--- my_file1.py
+++ my_file1.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title('Ejemplo de slider')
 n = st.slider('n', 5, 100, step = 1)
 char_data = pd.DataFrame(np.random.rand(n), columns = ['data'])
 st.line_chart(char_data)
